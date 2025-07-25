587028cdd952282e600328085a87097caedea0ba
Matt Boorman <boormanmatthew@gmail.com>
2025-07-25 11:11:44 +1000

    Integrated deck and card into app and set up some initial game state logic.

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,28 +1,82 @@
 
 import { Button } from "@/components/ui/button";
 import { TableDemo } from "../MBcomponents/shadcnComps";
+import { Deck, Card } from "@/scripts/cardDeck";
+import { Game, GameState, Player } from "@/scripts/gameLogic";
+
+let testCard = new Card("A", "Hearts");
+let testHand : Card[] = [new Card("K", "Hearts"), new Card("Q", "Hearts"), new Card("J", "Hearts")]
+
+let gameInstance = new Game();
+gameInstance.addPlayer(new Player("Player 1", false, false))
 
 export default function page () {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold mb-8">Welcome to the Game Page!</h1>
-      <p className="text-lg mb-4">This is a placeholder for your game content.</p>
-      <p className="text-md mb-8">Feel free to add interactive elements, game logic, or anything else you need here.</p>
-      <Button className="mb-8">Start Game</Button>
-      <Card suit="Hearts" rank="A"></Card>
+      <h1 className="text-4xl font-bold mb-8">Blackjack Game</h1>
+      <p className="text-lg mb-4">A game of Blackjack built with Next.js and Tailwind CSS.</p>
+      <p className="text-md mb-8">Hit the 'Start Game' button to start!</p>
+
+      <ControlPanel gameState={gameInstance.gameState}/>
+
+      <div className="flex flex-col items-center space-y-2 mb-4">
+        <CardProp card={testCard} />
+        <HandProp cards={testHand}/>
+      </div>
+
       <h2 className="text-2xl font-semibold mb-4">Game Stats (Example Table)</h2>
       <TableDemo />
     </div>
   )
 }
 
-interface CardProps {
-  suit: string;
-  rank: string;
+export function ControlPanel({gameState}: {gameState: GameState}) {
+
+    console.log(gameState)
+    let content;
+
+    switch (gameState) {
+        case "init": 
+            content = 
+                <>
+                    <Button className="mb-8">Start Game</Button>
+                </>;
+            break;
+
+        case "playerTurn":
+            content =
+                <>
+                    <Button className="mb-8">Hit</Button>
+                    <Button className="mb-8">Stand</Button>
+                </>
+            break;
+        default:
+            content = ''
+            console.log('not working')
+    }
+
+    return (
+        <div className="flex flex-row items-center space-x-4">
+            {content}
+        </div>
+    )
 }
 
-export function Card({ suit, rank }: CardProps) {
-  const cardColor = (suit === "Hearts" || suit === "Diamonds") ? "text-red-600" : "text-black";
+export function HandProp({ cards }: { cards: Card[] }) {
+  return (
+    <div className="flex flex-row -space-x-6">
+      {cards.map((card, index) => (
+        <CardProp key={index} card={card} />
+      ))}
+    </div>
+  );
+}
+
+export function CardProp({ card }: { card: Card }) {
+    let suit = card.suit;
+    let rank = card.rank;
+
+    const cardColor = (suit === "Hearts" || suit === "Diamonds") ? "text-red-600" : "text-black";
 
   return (
     <div className={`relative flex flex-col items-center justify-center w-20 h-28 border border-gray-400 rounded-md shadow-md bg-white ${cardColor}`}>
---
+++ src/scripts/gameLogic.ts
@@ -0,0 +1,295 @@
+import { error } from "console";
+import { Deck, Card } from "./cardDeck";
+//const prompt = require("prompt-sync")();
+
+type winState = 'won' | 'lost' | 'drew' | 'null';
+export type GameState = 'init' | 'playerTurn' | 'gameEnd';
+
+export class Player {
+
+    public hand: Card[] = [];
+    public score: number = 0;
+    public isBust: boolean = false;
+    public isBlackjack: boolean = false;
+    private winState: winState = 'null';
+
+    public handsWon: number = 0;
+
+    constructor(public readonly name: string, public readonly isDealer: boolean = false, public readonly isCPU: boolean = true) {
+    
+    }
+
+    resetHand(): void {
+        this.hand = [];
+        this.score = 0;
+        this.isBust = false;
+        this.isBlackjack = false;
+        this.winState = 'null';
+    }
+
+    drawCards(deck: Deck, numCards: number): void {
+
+        //draw cards from the deck and log them
+        let dealtCards = deck.dealCards(numCards);
+
+        dealtCards.forEach(card => {
+            console.log(this.name + ' drew a ' + card.toString());
+        });
+        
+        //Add the cards to the player's hand and check their new score
+        this.hand = this.hand.concat(dealtCards);
+
+        //Update the player's score and log if bust or blackjack
+        this.checkScore();
+
+        if (this.isBust) {
+            console.log(this.name + ' is bust!');
+        }
+
+        if (this.isBlackjack) {
+            console.log(this.name + ' is blackjack!');
+        }
+
+    }
+
+    //Calculate the player's hand score
+    //TO DO: Write Unit Test
+    private checkScore(): void {
+
+        if(this.isBlackjack || this.isBust) {
+            return;
+        }
+
+        //Reset score to recalculate
+        this.score = 0;
+
+        let aceCount = 0;
+
+        for (let i = 0; i < this.hand.length; i++) {
+
+            //Count aces and add score for other cards
+            if (this.hand[i].rank === 'Ace') {
+                aceCount++;
+            }
+            else {
+                this.score += this.hand[i].getCardValue();
+            }
+        }
+
+        //Logic to deal with aces (if any are held)
+        if(aceCount > 0) {
+
+            //If theres room for one ace as '11' and all the rest as '1'
+            if (11 + aceCount-1 + this.score <= 21) {
+                this.score += 11 + aceCount-1;
+            }
+            //Otherwise count all aces as '1's
+            else {
+                this.score += aceCount;
+            }         
+        }
+
+        //Check for bust and blackjack once score is set
+        if (this.score > 21) {
+            this.isBust = true;
+        }
+        else if (this.score === 21) {
+            this.isBlackjack = true;
+        }
+
+        console.log('   = ' + this.score + ' (' + this.name + ')')
+    }
+
+    public setWinState(state: winState): void {
+        
+        this.winState = state;
+
+        if(state === 'won') {
+            this.handsWon++;
+        }
+    }
+
+    public getWinState(): winState {
+        return this.winState;
+    }  
+}
+
+export class Game {
+
+    public players: Player[] = [];
+    public dealer: Player = new Player('Dealer', true);
+    public gameState: GameState = 'init';
+
+    public deck: Deck = new Deck();
+    public roundNumber: number = 0;
+
+
+
+    constructor() {
+    }
+
+    addPlayer(player: Player): void {
+        this.players.push(player);
+    }
+
+    resetGame(): void {
+        this.players.forEach(player => player.resetHand());
+        this.dealer.resetHand();
+        this.deck = new Deck();
+        this.deck.shuffle();
+    }
+
+    roundScoreboard(): void {
+        console.log('--- ROUND ' + this.roundNumber + ' SCORES ---')
+        console.log('Dealer: ' + this.dealer.score);
+        this.players.forEach(player => console.log(player.name + ': ' + player.score + ' (' + player.getWinState() + ')'));
+    }
+
+    gameScoreboard(): void {
+    
+        console.log('--- GAME SCORES ---')
+        this.players.forEach(player => console.log(player.name + ' has won ' + player.handsWon + ' hands.'))
+        console.log('----------');
+    
+    }
+
+    public playRound(): void {
+        
+        this.resetGame();
+        this.roundNumber++;
+
+        console.log('----------');
+        console.log('**Round ' + this.roundNumber + '**');
+
+        //Round starts by dealing two cards to each player and dealer
+        console.log('--- INITIAL DRAW ---');
+        this.players.forEach(player => player.drawCards(this.deck,2));
+        this.dealer.drawCards(this.deck,2);
+
+        //Players' turns
+        console.log('--- PLAYER TURNS ---');
+        this.players.forEach(player =>{
+            this.playerTurn(player);
+            //Only add spacer if it's not the last player
+            if(!(this.players.findIndex(p => p === player)+1 === this.players.length)) {
+                console.log('--');
+            }
+        })
+
+        //Dealer's Turn
+        console.log("--- DEALER'S TURN ---");
+        this.dealersTurn();
+
+        this.calculateWinners();
+        this.roundScoreboard();
+
+        console.log('----------');
+
+    }
+
+    private playerTurn(player: Player): void {
+
+        if(player.isCPU) {
+            while(player.score < 17){
+                player.drawCards(this.deck,1);
+            }
+            if (!player.isBust && !player.isBlackjack) {
+                console.log(player.name + ' Stands on ' + player.score + '.');
+            }      
+        }
+
+        else {
+
+            let continueTurn = true;
+
+            console.log('Your turn (' + player.name + ')' + ' - ' + player.score + '.')
+
+            while (continueTurn) {
+
+                //Ask player to hit or stand
+                if (prompt('Hit (y) or Stand?: ') === 'y') {
+                    player.drawCards(this.deck, 1);
+                }
+                else {
+                    continueTurn = false;
+                }
+
+                //If player is bust or has blackjack auto-end their turn.
+                if(player.isBust || player.isBlackjack) {
+                    continueTurn = false;
+                }
+
+            }
+
+            //Log 'stands' if turn ends without bust or blackjack.
+            if (!player.isBust && !player.isBlackjack) {
+                console.log(player.name + ' stands on ' + player.score + '.');
+            }
+        }      
+    }
+
+    private dealersTurn(): void {
+
+        //In case dealer gets blackjack on the first cards (note bust is not possible)
+        if (this.dealer.isBlackjack) {
+            console.log('Dealer is blackjack!')
+            return;
+        }
+
+        while (this.dealer.score < 17) {
+            this.dealer.drawCards(this.deck, 1);
+        }
+        if (!this.dealer.isBust && !this.dealer.isBlackjack) {
+            console.log('Dealer Stands on ' + this.dealer.score + '.')
+        }
+    }
+
+    private calculateWinners(): void {
+
+        //When dealer is blackjack
+        if (this.dealer.isBlackjack) {
+           this.players.forEach(player => {
+                if (player.isBlackjack) {
+                    player.setWinState('drew');
+                }
+                else {
+                    player.setWinState('lost');
+                }
+           });
+        }
+        //when dealer is bust
+        else if (this.dealer.isBust) {
+            this.players.forEach(player => {
+                if (!player.isBust) {
+                    player.setWinState('won');
+                }
+                else {
+                    player.setWinState('lost');
+                }
+            });
+        }
+
+        //when dealer is neither blackjack or bust
+        else {
+            this.players.forEach(player => {
+                if (player.isBlackjack) {
+                    player.setWinState('won');
+                }
+                else if (player.isBust) {
+                    player.setWinState('lost');
+                }
+                else if (player.score === this.dealer.score) {
+                    player.setWinState('drew');
+                }
+                else if (player.score > this.dealer.score) {
+                    player.setWinState('won');
+                }
+                else {
+                    player.setWinState('lost');
+                }
+            })
+        }
+
+    }
+    
+}
